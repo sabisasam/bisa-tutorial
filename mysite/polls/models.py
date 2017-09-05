216121be78445996f03c281936e62faea292c0f8
Sabine <sabisasam@users.noreply.github.com>
2017-09-05 16:01:39 +0200

    Went on with "Getting Started with Channels" (#15). Two chapters remaining.
    Next chapter: https://channels.readthedocs.io/en/stable/getting-started.html#enforcing-ordering

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -59,6 +59,18 @@ class QuestionHistory(models.Model):
             )
 
 
+class Room(models.Model):
+    name = models.TextField()
+    label = models.SlugField(unique=True)
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages')
+    handle = models.TextField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+
+
 @receiver(post_migrate)
 def add_permissions(sender, **kwargs):
     """
