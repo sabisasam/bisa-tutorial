f0589c50b6baabe30c16e6d469e763d26fe6e98c
Sabine <sabisasam@users.noreply.github.com>
2017-10-24 18:48:01 +0200

    Edited files in fortune/cookies which were causing an UnicodeDecodeError.
    In all files the error was caused through the ” symbol (“ symbol was okay). shlomif also had a problem with some kind of ‐ symbol.

--- mysite/fortune/views.py
+++ mysite/fortune/views.py
@@ -5,27 +5,20 @@ from fortune.models import Category, get_available_pack_names, PackAlreadyLoaded
 
 def index(request):
     """
-    Belongs to the Fortune page which shows a quote.
+    Belongs to the Fortune page which shows a quote, a saying or something similar.
     """
-    trouble = ['arrested',
-               'codehappy',
+    trouble = ['codehappy',
                'commandlinefu',
                'community',
                'firefly',
-               'futurama',
                'gems',
-               'joel-on-software',
-               'oneliners',
-               'osp_rules',
-               'parksandrec',
-               'paul-graham',
-               'shlomif',
-               'shlomif-fav'] # Those are causing UnicodeDecodeError or IntegrityError.
-    packs = get_available_pack_names()
-    packs = [pack for pack in packs if not pack.endswith('.dat') and not pack.endswith('.pdat')]
-    packs = [pack for pack in packs if pack not in trouble]
+               'oneliners']
+               # IntegrityError: UNIQUE constraint failed: fortune_fortune.category_id, fortune_fortune.text
+    #packs = get_available_pack_names()
+    #packs = [pack for pack in packs if not pack.endswith('.dat') and not pack.endswith('.pdat')]
+    #packs = [pack for pack in packs if pack not in trouble]
+    packs = ['shlomif-fav']
     for pack in packs:
-        print(pack)
         try:
             Category.load(pack)
         except PackAlreadyLoadedError:
