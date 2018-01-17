4e2b4dca1b23dcc9c246d185f93b1164f657cda9
Peter (dkdndes) <dkdndes@gmail.com>
2018-01-17 17:23:38 +0100

    fixed pep8 errors

--- tutorial/fortune/rabbitmq/rpc_server.py
+++ tutorial/fortune/rabbitmq/rpc_server.py
@@ -5,49 +5,56 @@ import pika
 
 # Establish connection with RabbitMQ server (broker on given machine (here: localhost)).
 # (Connect to a broker on a different machine by specifying its name or IP address here.)
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost'))
 channel = connection.channel()
 
 # Make sure that queue exists (like "get or create", queue_declare is idempotent).
-# Show existing queues and how many messages are in them with '$ rabbitmqctl list_queues'.
+# Show existing queues and how many messages are in them with '$
+# rabbitmqctl list_queues'.
 channel.queue_declare(queue='rpc_queue')
 
 # Declaring fibonacci function, assumes only valid positive integer input.
+
+
 def fib(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2)
 
 # Declaring a callback for 'basic_consume', the core of the RPC server.
 # Gets called when a request message is received.
+
+
 def on_request(ch, method, props, body):
     n = int(body)
     print(" [.] fib(%s)" % n)
     response = fib(n)
 
     # Send response message with result to exchange.
-    ch.basic_publish(exchange='', # Default exchange, routes messages to queue specified
-                                  # by routing_key if it exists.
-                     routing_key=props.reply_to, # Queue name.
-                                                 # (Using callback queue given from client.)
-                     properties=pika.BasicProperties( # Message properties.
-                                                      # The AMQP 0-9-1 protocol predefines
-                                                      # a set of 14 properties that go with
-                                                      # a message. Most of them are rarely
-                                                      # used, with the exception of
-                                                      # 'delivery_mode' (marks a message as
-                                                      # persistent (with a value of 2) or
-                                                      # transient (any other value)),
-                                                      # 'content_type' (used to describe
-                                                      # the mime-type of the encoding),
-                                                      # 'reply_to' (commonly used to name a
-                                                      # callback queue) and 'correlation_id'.
-                        correlation_id = props.correlation_id), # Useful to correlate RPC
-                                                                # responses with requests.
-                                                                # (Will be set to a unique
+    ch.basic_publish(exchange='',  # Default exchange, routes messages to queue specified
+                     # by routing_key if it exists.
+                     routing_key=props.reply_to,  # Queue name.
+                     # (Using callback queue given from client.)
+                     properties=pika.BasicProperties(  # Message properties.
+                         # The AMQP 0-9-1 protocol predefines
+                         # a set of 14 properties that go with
+                         # a message. Most of them are rarely
+                         # used, with the exception of
+                         # 'delivery_mode' (marks a message as
+                         # persistent (with a value of 2) or
+                         # transient (any other value)),
+                         # 'content_type' (used to describe
+                         # the mime-type of the encoding),
+                         # 'reply_to' (commonly used to name a
+                         # callback queue) and 'correlation_id'.
+                         correlation_id=props.correlation_id),  # Useful to correlate RPC
+                     # responses with requests.
+                     # (Will be set to a unique
                                                                 # value for every request so
                                                                 # we will be able to match a
                                                                 # response with a request.)
@@ -55,10 +62,11 @@ def on_request(ch, method, props, body):
                                                                 # correlation_id don't belong
                                                                 # to our requests and will be
                                                                 # discarded.)
-                     body=str(response)) # Message.
+                     body=str(response))  # Message.
 
     # Send acknowledgment that request message had been received and processed.
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 # Tell RabbitMQ not to give more than one message to a server at a time.
 # RabbitMQ will dispatch the message to the next server that is not still busy.
@@ -70,5 +78,6 @@ channel.basic_consume(on_request,
                       queue='rpc_queue')
 
 print(" [x] Awaiting RPC requests")
-# Enter never-ending loop that waits for data and runs callbacks whenever necessary.
+# Enter never-ending loop that waits for data and runs callbacks whenever
+# necessary.
 channel.start_consuming()
--- tutorial/fortune/rabbitmq/worker.py
+++ tutorial/fortune/rabbitmq/worker.py
@@ -6,7 +6,9 @@ import time
 
 # Establish connection with RabbitMQ server (broker on given machine (here: localhost)).
 # (Connect to a broker on a different machine by specifying its name or IP address here.)
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost'))
 channel = connection.channel()
 
 # Make sure that queue exists (like "get or create", queue_declare is idempotent).
@@ -17,12 +19,16 @@ print(' [*] Waiting for messages. To exit press CTRL+C (Unix) or CTRL+BREAK (Win
 
 # Declaring a callback for 'basic_consume'.
 # Gets called when a message is received.
+
+
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-    time.sleep(body.count(b'.')) # Fake a second of work for every dot in the message.
+    # Fake a second of work for every dot in the message.
+    time.sleep(body.count(b'.'))
     print(" [x] Done")
     # Send acknowledgment that message had been received and processed.
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
 
 # Tell RabbitMQ not to give more than one message to a worker at a time.
 # RabbitMQ will dispatch the message to the next worker that is not still busy.
@@ -30,9 +36,11 @@ def callback(ch, method, properties, body):
 # (Default behaviour: sends each message to next consumer in sequence (round-robin).)
 channel.basic_qos(prefetch_count=1)
 # Specify which function should receive messages from which queue (subscribing to queue).
-# Default: message achnowledgments are turned on (important: don't forget basic_ack!).
+# Default: message achnowledgments are turned on (important: don't forget
+# basic_ack!).
 channel.basic_consume(callback,
                       queue='task_queue')
 
-# Enter never-ending loop that waits for data and runs callbacks whenever necessary.
+# Enter never-ending loop that waits for data and runs callbacks whenever
+# necessary.
 channel.start_consuming()
