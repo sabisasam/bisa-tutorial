b212a30aeb8bec81ab766a65dacd27b8e5472c41
Peter (dkdndes) <dkdndes@gmail.com>
2018-01-17 17:02:32 +0100

    added DEBUG default==True for tutorials

--- tutorial/main/settings.py
+++ tutorial/main/settings.py
@@ -23,13 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'chat.apps.ChatConfig',
     'fortune.apps.FortuneConfig',
