c8e00fe0e607a2cb1a02e9566825a35fb6bb5420
Sabine <sabisasam@users.noreply.github.com>
2018-01-19 17:11:40 +0100

    Hopefully resolving conflicts in urls.py now

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^webhook/', include('webhook.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^fortune/', include('fortune.urls')),
     url(r'^chat/', include('chat.urls')),
