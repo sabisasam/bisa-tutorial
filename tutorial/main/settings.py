a01123f66711b8e02cd56a1917e1137ea7880dd4
Sabine <sabisasam@users.noreply.github.com>
2018-01-19 19:04:51 +0100

    Resolving merge conflicts from merging Feature/Webhook into develop

--- tutorial/main/settings.py
+++ tutorial/main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,17 +21,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
+GITHUB_WEBHOOK_KEY = config('GITHUB_WEBHOOK_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
     'chat.apps.ChatConfig',
     'fortune.apps.FortuneConfig',
     'polls.apps.PollsConfig',
+    'webhook.apps.WebhookConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
